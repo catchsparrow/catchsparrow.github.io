af5e423302ff5cbbd39c1057d6227b4aa9f815a8
catchsparrow <csparrow@massart.edu>
2016-04-22 14:45:52 -0400

    final site

---
+++ final/script.js
@@ -0,0 +1,12 @@
+function changeText(text)
+    {
+    var display = document.getElementById('text-display');
+    display.innerHTML = text;
+    }     
+    function defaultText(text)
+    {
+    var display = document.getElementById('text-display');
+    display.innerHTML = "projects";
+    }
+
+ 
