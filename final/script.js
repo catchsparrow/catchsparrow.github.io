40fa61600646b31fdb0d1a86cc133391eebb90a2
catchsparrow <csparrow@massart.edu>
2016-04-24 13:21:12 -0400

    back to before

--- final/script.js
+++ final/script.js
@@ -8,3 +8,17 @@
     var display = document.getElementById('text-display');
     display.innerHTML = "projects";
     }
+
+    function changeHead(text)
+        {
+            var display = document.getElementById('head');
+            display.innerHTML = "";
+            display.innerHTML = text;
+        }
+          function changeback(text)
+        {
+            var display = document.getElementById('head');
+            display.innerHTML = "riona";
+            display.innerHTML = text;
+        }
+
