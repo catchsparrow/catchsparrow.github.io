90f6598016a3c07b87f5c98f8c57c744c8cd7121
catchsparrow <csparrow@massart.edu>
2016-04-24 14:46:26 -0400

    more attempts

--- final/script.js
+++ final/script.js
@@ -8,17 +8,3 @@
     var display = document.getElementById('text-display');
     display.innerHTML = "projects";
     }
-
-    function changeHead(text)
-        {
-            var display = document.getElementById('head');
-            display.innerHTML = "";
-            display.innerHTML = text;
-        }
-          function changeback(text)
-        {
-            var display = document.getElementById('head');
-            display.innerHTML = "riona";
-            display.innerHTML = text;
-        }
-
