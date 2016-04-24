b412be196c764915a9fa6623abc6b6d2c505d2a2
catchsparrow <csparrow@massart.edu>
2016-04-24 12:36:32 -0400

    working with java script

--- final/script.js
+++ final/script.js
@@ -1,4 +1,18 @@
 	function changeText(text)
+        {
+            var display = document.getElementById('text-display');
+            display.innerHTML = "";
+            display.innerHTML = text;
+        }
+          function defaultText(text)
+        {
+            var display = document.getElementById('text-display');
+            display.innerHTML = "riona";
+            display.innerHTML = text;
+        }
+
+
+    function changeText(text)
     {
     var display = document.getElementById('text-display');
     display.innerHTML = text;
