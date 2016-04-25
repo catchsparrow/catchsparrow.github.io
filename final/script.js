db086123c7f015cc2c37c8a3e04f5b1dfdbf0c06
catchsparrow <csparrow@massart.edu>
2016-04-25 12:20:09 -0400

    dobot font

--- final/script.js
+++ final/script.js
@@ -9,16 +9,3 @@
     display.innerHTML = "projects";
     }
 
-var toggle = 0;
-
-$("button").on("click", function() {
-  
-  if (toggle) {
-    $("body").css("font-size", "36px");
-    toggle = 0;
-  } else {
-    $("body").css("font-size", "16px");
-    toggle = 1;
-  }
-  
-});
