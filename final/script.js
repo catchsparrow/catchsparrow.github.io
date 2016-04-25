4f453333e317ab9cfc873870ad59b2f4ff7be39a
catchsparrow <csparrow@massart.edu>
2016-04-25 09:18:17 -0400

    more changes

--- final/script.js
+++ final/script.js
@@ -8,3 +8,17 @@
     var display = document.getElementById('text-display');
     display.innerHTML = "projects";
     }
+
+var toggle = 0;
+
+$("button").on("click", function() {
+  
+  if (toggle) {
+    $("body").css("font-size", "36px");
+    toggle = 0;
+  } else {
+    $("body").css("font-size", "16px");
+    toggle = 1;
+  }
+  
+});
