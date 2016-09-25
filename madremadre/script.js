c141e78f854d91f1f1252ea291618a6b3e20f05e
catchsparrow <csparrow@massart.edu>
2016-09-25 13:20:45 +0100

    css

---
+++ madremadre/script.js
@@ -0,0 +1,38 @@
+function changeText(text) {
+  var display = document.getElementById('text-display');
+  display.innerHTML = text;
+}
+
+    function changeDesc(text)
+    {
+    var display = document.getElementById('desc-display');
+    display.innerHTML = text;
+    }  
+
+    function defaultText(text)
+    {
+    var display = document.getElementById('text-display');
+    display.innerHTML = "";
+    }
+    function defaultDesc(text)
+    {
+    var display = document.getElementById('desc-display');
+    display.innerHTML = "";
+    }
+
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
